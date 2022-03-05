5ba771fe96d330f64957cd44f0050303f4ce1220
Samile Leite <samileleite77@gmail.com>
2022-03-05 11:07:08 -0300

    first commit

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc';
+import { AuthContext } from '../../context/auth';
+
+import styles from './styles.module.scss';
+
+export function LoginBox() {
+    const { signInUrl } = useContext(AuthContext);
+    // console.log(user);
+
+    return (
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href={signInUrl} className={styles.signInWithGithub}>
+                <VscGithubInverted size="24" />
+                Entrar com Github
+            </a>
+        </div>
+    )
+}
